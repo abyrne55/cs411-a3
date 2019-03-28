4114aa982a02655ee047af721f983dd25c11ee97
chenliu <cliu2015@bu.edu>
2019-03-28 14:29:01 -0400

    change

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,10 @@
 var express = require('express');
 var router = express.Router();
+var playlist = require('../models/playlist');
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { results: ['PL1', 'PL2'] });
+  res.render('index', { results: [playlist.findPlaylist] });
 });
 
 module.exports = router;
