3dfaf68141762be345fd17e174b2b3c80a477afb
chenliu <cliu2015@bu.edu>
2019-03-28 23:32:23 -0400

    change

--- models/access_token.js
+++ models/access_token.js
@@ -3,7 +3,7 @@ var SpotifyWebApi = require('spotify-web-api-node');
 var clientId = '595baad9a3584651a41e0be0e8813420';
 var clientSecret = '9bbb86e9414341fc9d5c63a782f4c668';
 
-// credentials are optional
+// credentials are optional and from https://www.npmjs.com/package/spotify-web-api-node
 var spotifyApi = new SpotifyWebApi({
     clientId: clientId,
     clientSecret: clientSecret
@@ -16,7 +16,7 @@ function getToken() {
             console.log('The access token expires in ' + data.body['expires_in']);
             console.log('The access token is ' + data.body['access_token']);
 
-            // Save the access token
+            // Set the access token on the API object to use it in later calls
             spotifyApi.setAccessToken(data.body['access_token']);
         },
         function (err) {
