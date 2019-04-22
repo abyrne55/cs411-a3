90764e60c86ed432a961d1f8c5f0bd38d55fecbe
dynnorshebshaievitz <dynnor@bu.edu>
2019-04-21 21:27:49 -0400

    test

--- routes/about.js
+++ routes/about.js
@@ -3,9 +3,8 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  /* [['playlist-link', 'Playlist-name'], ['playlist-link', 'playlist-name']] */
-  res.render('index', { results: [] });
+    /* [['playlist-link', 'Playlist-name'], ['playlist-link', 'playlist-name']] */
+    res.render('about', { results: [] });
 });
-
 
 module.exports = router;
